1f28211b92409076b28c9406fe8675cf4f73d8b0
oolralra <oolralra@gmail.com>
2024-02-13 10:46:43 +0900

    Create locals.tf

---
+++ eks_vpc/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  subnet_tags = {
+    public   = lookup(var.subnet_tags, "public", null)
+    private  = lookup(var.subnet_tags, "private", null)
+    database = lookup(var.subnet_tags, "database", null)
+  }
+
+  default_tags = {
+    env        = var.env
+    managed_by = "terraform"
+  }
+}
